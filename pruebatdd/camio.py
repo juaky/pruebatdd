49744439952fa192acad8487f760dd8207a9c96c
juaky <juaky73@gmail.com>
2020-06-30 07:49:05 +0200

    Inicial -> test fail

---
+++ pruebatdd/camio.py
@@ -0,0 +1,13 @@
+class Camio(object):
+    def add(self):
+        pass
+
+    def substract(self):
+        pass
+
+    def quantitatLlet(self):
+        pass
+
+    def controlaPes(self):
+        pass
+
---
+++ pruebatdd/test_camio.py
@@ -0,0 +1,15 @@
+from twisted.trial import unittest
+from pruebatdd.camio import Camio
+
+class TestCamio(unittest.TestCase):
+    def test_add(self):
+        self.fail()
+
+    def test_substract(self):
+        self.fail()
+
+    def test_quantitatLlet(self):
+        self.fail()
+
+    def test_controlaPes(self):
+        self.fail()
